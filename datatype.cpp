f2b7517ed30d2b257bdb2c1cbb3812eb498a28fc
Ashish Kumar <72808868+ashish-kumar-7@users.noreply.github.com>
2021-10-01 00:47:59 +0530

    size of different datatype

---
+++ datatype.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	cout << "Hello World\n";
+	int a;
+	float b;
+	char c;
+	bool d;
+	short int si;
+	long int li;
+	cout << sizeof(a)<<endl;
+	cout << sizeof(b)<<endl;
+	cout << sizeof(c)<<endl;
+	cout << sizeof(d)<<endl;
+	cout << sizeof(si)<<endl;
+	cout << sizeof(li)<<endl;
+	return 0;
+}
